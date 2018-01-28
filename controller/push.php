e48f9ee921571286b745adb159a78fd565e57e9b
olandachocobear <larut0@yahoo.com>
2018-01-28 13:51:41 +0700

    add actionable buttons on Schedule-type notif
    *) rename functions on Push class

--- controller/push.php
+++ controller/push.php
@@ -47,7 +47,7 @@ class Push
 
 	}
 
-	public function new_msg_notif($msg_id, $img=null, $sound=null) {
+	public function msg_notif($msg_id, $img=null, $sound=null) {
 		try
 		{
 			$stmt = $this->conn->prepare("SELECT m.*,t.kode_barkode FROM tb_detail_push m INNER JOIN tb_push p on p.kd_push = m.kd_push INNER JOIN tb_karyawan k on k.no_KTP = p.kepada  LEFT JOIN tb_karyawan_mobile t ON t.id = k.id WHERE m.kd_push = :kode");
@@ -83,7 +83,7 @@ class Push
 		}
 	}
 
-	public function new_interview_notif($intrvw_id, $img=null, $sound=null) {
+	public function interview_notif($intrvw_id, $img=null, $sound=null) {
 		try
 		{
 			$stmt = $this->conn->prepare("SELECT i.*,m.kode_barkode FROM tb_info_interview i INNER JOIN tb_karyawan k on k.no_ktp = i.no_ktp INNER JOIN tb_karyawan_mobile m ON m.id = k.id WHERE kd_interview = :kode");
@@ -126,7 +126,7 @@ class Push
 		}
 	}
 
-	public function new_test_notif($test_id, $img=null, $sound=null) {
+	public function test_notif($test_id, $img=null, $sound=null) {
 		try
 		{
 			$stmt = $this->conn->prepare("SELECT t.*,m.kode_barkode FROM tb_info_test t INNER JOIN tb_karyawan k on k.no_ktp = t.no_ktp INNER JOIN tb_karyawan_mobile m ON m.id = k.id WHERE kode_test = :kode");
@@ -159,6 +159,145 @@ class Push
 				$msg_payload->soundname = $this->getSound($sound); // changeable!!
 
 			$target = $result[0]['kode_barkode'];
+
+			return $this->Firebase->SendNotification($msg_payload, $target);
+			 
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	public function interview_notif_w_actions($intrvw_id, $img=null, $sound=null) {
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT i.*,m.kode_barkode FROM tb_info_interview i INNER JOIN tb_karyawan k on k.no_ktp = i.no_ktp INNER JOIN tb_karyawan_mobile m ON m.id = k.id WHERE kd_interview = :kode");
+
+			$stmt->execute(array(':kode'=>$intrvw_id));
+
+			$result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			// prepping standard Notif contents...
+			$msg_payload = (object)[];
+			$msg_payload->title = TITLE_NEW_SCHEDULE;
+			$msg_payload->message = 'Interview, Tgl: ' . $result[0]['date_interview'];
+			$msg_payload->message .= " Harap konfirm Jam ketersediaan di bawah:";
+			$msg_payload->notId = rand(1,1000);
+
+			// additional data specific to schedule
+			$msg_payload->notif_type = 'schedule';
+			$msg_payload->schedule_type = 'interview';
+			$msg_payload->schedule_note = $result[0]['detail'];
+			$msg_payload->schedule_date = $result[0]['date_interview'];
+			$msg_payload->schedule_id = $result[0]['kd_interview'];
+
+			// Set Actions 
+			$actions = json_decode( '[{
+					
+					"title": "Jam 10 Pagi",
+					"callback": "confirm10AM",
+					"foreground": true
+				},{
+					
+					"title": "Jam 2 Siang",
+					"callback": "confirm14PM",
+					"foreground": true
+				},{
+					
+					"title": "Hubungi Admin",
+					"callback": "contactAdmin",
+					"foreground": false
+				}]
+			');
+
+			$msg_payload->actions = $actions;
+
+			// required to always put on top & show Action options on notification drawer..
+			$msg_payload->priority = 2;
+
+			// Set icons
+			if (isset($img))
+				$msg_payload->image = $img;
+			else
+				$msg_payload->image = ICON_NEW_SCHEDULE;
+
+			// Set sound
+			if (isset($sound))
+				$msg_payload->soundname = $this->getSound($sound); // changeable!!
+
+			$target = $result[0]['kode_barkode'];
+
+			return $this->Firebase->SendNotification($msg_payload, $target);
+			 
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	public function test_notif_w_actions($test_id, $img=null, $sound=null) {
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT t.*,m.kode_barkode FROM tb_info_test t INNER JOIN tb_karyawan k on k.no_ktp = t.no_ktp INNER JOIN tb_karyawan_mobile m ON m.id = k.id WHERE kode_test = :kode");
+
+			$stmt->execute(array(':kode'=>$test_id));
+
+			$result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			// prepping standard Notif contents...
+			$msg_payload = (object)[];
+			$msg_payload->title = TITLE_NEW_SCHEDULE;
+			$msg_payload->message = 'Test, Tgl: ' . $result[0]['date_test'];
+			$msg_payload->message .= " Harap konfirm Jam ketersediaan di bawah:";
+			$msg_payload->notId = rand(1,1000);
+
+			// additional data specific to schedule
+			$msg_payload->notif_type = 'schedule';
+			$msg_payload->schedule_type = 'test';
+			$msg_payload->schedule_note = $result[0]['keterangan'];
+			$msg_payload->schedule_date = $result[0]['date_test'];
+			$msg_payload->schedule_id = $result[0]['kode_test'];
+
+			// Set Actions 
+			$actions = json_decode( '[{
+					
+					"title": "Jam 10 Pagi",
+					"callback": "confirm10AM",
+					"foreground": true
+				},{
+					
+					"title": "Jam 2 Siang",
+					"callback": "confirm14PM",
+					"foreground": true
+				},{
+					
+					"title": "Hubungi Admin",
+					"callback": "contactAdmin",
+					"foreground": false
+				}]
+			');
+
+			$msg_payload->actions = $actions;
+
+			// required to always put on top & show Action options on notification drawer..
+			$msg_payload->priority = 2;
+
+			// Set icons
+			if (isset($img))
+				$msg_payload->image = $img;
+			else
+				$msg_payload->image = ICON_NEW_SCHEDULE;
+
+			// Set sound
+			if (isset($sound))
+				$msg_payload->soundname = $this->getSound($sound); // changeable!!
+
+			$target = $result[0]['kode_barkode'];
+
+			// debug!
+			echo json_encode( $msg_payload);
 
 			return $this->Firebase->SendNotification($msg_payload, $target);
 			 
--- push/test/index.php
+++ push/test/index.php
@@ -8,13 +8,13 @@ $push = new Push();
 
 if (isset($_GET['icon']))
 	if (isset($_GET['sound']))
-		respond ($push->new_test_notif($_GET['id'],$_GET['icon'],$_GET['sound']));
+		respond ($push->test_notif_w_actions($_GET['id'],$_GET['icon'],$_GET['sound']));
 	else
-		respond ($push->new_test_notif($_GET['id'],$_GET['icon']));
+		respond ($push->test_notif_w_actions($_GET['id'],$_GET['icon']));
 else if(isset($_GET['sound']))
-	respond ($push->new_test_notif($_GET['id'],null,$_GET['sound']));
+	respond ($push->test_notif_w_actions($_GET['id'],null,$_GET['sound']));
 else
-	respond ($push->new_test_notif($_GET['id']));
+	respond ($push->test_notif_w_actions($_GET['id']));
 	
 
 ?>
